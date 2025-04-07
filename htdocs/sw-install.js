98e90282c7a06f4a97d7014b85934505f1dec7c5
AkramKhan16 <165643198+AkramKhan16@users.noreply.github.com>
2025-04-07 16:17:36 +0530

    third

--- htdocs/sw-install.js
+++ htdocs/sw-install.js
@@ -1,11 +1,9 @@
-// Service worker install code goes in here!
-// sw-install.js
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-    .then(function (registration) {
-      console.log('Service Worker registered with scope:', registration.scope);
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("sw.js")
+    .then(function(registration) {
+      console.log("Service Worker registered with scope:", registration.scope);
     })
-    .catch(function (error) {
-      console.log('Service Worker registration failed:', error);
+    .catch(function(error) {
+      console.error("Service Worker registration failed:", error);
     });
 }
